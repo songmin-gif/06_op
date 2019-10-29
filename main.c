e27236dfa55f538132b71ce9b7ee710298f3d6e7
songmin-gif <55427684+songmin-gif@users.noreply.github.com>
2019-10-29 12:40:42 +0900

    06final

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+ 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#include <stdio.h> 
-int square(int n);
+int calCombination(int n, int r)
+{
+	int high, low;
+	high = factorial(n);
 
-int main (void)
-{
-	int result;
-	result= square(5);
-	printf("%d",result);
-}
-int square(int n) 
-{
- return (n * n); 
+	low = factorial(n-r)*factorial(r);
+
+	return (high/low);
 }
 
+int factorial(int n)
+{
+
+	int res=1;
+	int i;
+	
+	for(i=1; i<=n; i++)
+		res=res*i;
+		
+	return res;
+}
+int main(int argc, char* argv[])
+{
+	int n,r;
+	int res;
+		
+	printf("input n and r");
+	scanf("%d, %d", &n, &r);
+	
+	res=calCombination(n,r);
+	printf("combination result is %d\n", res);
+
+return 0;
+}
